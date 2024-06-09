e78a25d9bc0ca8ad6c6a7f02311526eb081bd7ee
sfatihy <seyitfatihyazici11@gmail.com>
2024-06-09 18:03:05 +0300

    CustomColumn widget fixed.

--- lib/widgets/column/custom_column_widget.dart
+++ lib/widgets/column/custom_column_widget.dart
@@ -4,7 +4,6 @@ class CustomColumn extends StatelessWidget {
 
   final EdgeInsets? padding;
   final ScrollPhysics? physics;
-  final bool shrinkWrap;
   final Axis scrollDirection;
   final List<Widget> children;
   /// It helps to adjust the space between children widgets.
@@ -14,7 +13,6 @@ class CustomColumn extends StatelessWidget {
     super.key,
     this.padding,
     this.physics,
-    this.shrinkWrap = false,
     this.scrollDirection = Axis.vertical,
     required this.children,
     required this.gap
@@ -22,14 +20,22 @@ class CustomColumn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
+    return SingleChildScrollView(
       padding: padding ?? EdgeInsets.zero,
       physics: physics,
-      shrinkWrap: shrinkWrap,
       scrollDirection: scrollDirection,
-      itemCount: children.length,
-      itemBuilder: (context, index) => children[index],
-      separatorBuilder: (context, index) => gap,
+      child: Column(
+        children: List.generate(
+          children.length,
+          (index) => Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              children[index],
+              index != children.length - 1 ? gap : const SizedBox()
+            ],
+          )
+        ),
+      ),
     );
   }
 }
