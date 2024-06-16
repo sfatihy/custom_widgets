5375a479abace7dfe3e1a0d3359361d77cdf41e9
sfatihy <seyitfatihyazici11@gmail.com>
2024-06-17 00:28:47 +0300

    PriceWidget added.

---
+++ lib/widgets/price/price_widget.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class PriceWidget extends StatelessWidget {
+
+  double price;
+  double? discountedPrice;
+  String currency;
+
+  PriceWidget({
+    super.key,
+    required this.price,
+    this.discountedPrice,
+    required this.currency
+  }) : assert(discountedPrice == null || discountedPrice < price, 'The discountedPrice field cannot be more greater than the price field');
+
+  String priceModifier(double price) {
+    String priceStr = price.toStringAsFixed(2);
+
+    List<String> parts = priceStr.split('.');
+    String integerPart = parts[0];
+
+    for (int i = integerPart.length - 3; i > 0; i -= 3) {
+      integerPart = '${integerPart.substring(0, i)},${integerPart.substring(i)}';
+    }
+
+    return '$integerPart.${parts[1]}';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+
+        Text(
+          '$currency${discountedPrice != null ? priceModifier(discountedPrice ?? 0).split('.').first : priceModifier(price).split('.').first}',
+          style: const TextStyle(
+            fontSize: 26,
+            fontWeight: FontWeight.w300,
+            height: 0,
+          ),
+        ),
+
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+            discountedPrice != null ? Text(
+              priceModifier(price),
+              style: const TextStyle(
+                fontSize: 10,
+                color: Colors.black45,
+                fontWeight: FontWeight.w600,
+                decorationColor: Colors.black45,
+                decorationStyle: TextDecorationStyle.solid,
+                decoration: TextDecoration.lineThrough,
+                height: 0
+              ),
+            ) : const SizedBox(),
+
+            Text(
+              '.${discountedPrice != null ? priceModifier(discountedPrice ?? 0).split('.').last : priceModifier(price).split('.').last}',
+              style: TextStyle(
+                fontSize: discountedPrice != null ? 16 : 20,
+                fontWeight: FontWeight.w300,
+                height: 0
+              ),
+            ),
+
+          ],
+        )
+
+      ],
+    );
+  }
+}
