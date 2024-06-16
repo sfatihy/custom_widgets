35de3fff3bcbfa5285506923ac9b4cc3125f0853
sfatihy <seyitfatihyazici11@gmail.com>
2024-06-16 23:21:00 +0300

    Added padding property to CustomDividerWidget

--- lib/widgets/divider/custom_divider_widget.dart
+++ lib/widgets/divider/custom_divider_widget.dart
@@ -5,40 +5,47 @@ class CustomDivider extends StatelessWidget {
   final Color? lineColor;
   final double? lineWidth;
   final Color? circleBorderColor;
+  final EdgeInsets? dividerPadding;
 
   const CustomDivider({
     super.key,
     this.lineColor,
     this.lineWidth,
-    this.circleBorderColor
+    this.circleBorderColor,
+    this.dividerPadding,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        Container(
-          width: lineWidth ?? MediaQuery.of(context).size.width * 0.35,
-          height: (lineWidth ?? MediaQuery.of(context).size.width * 0.35) * 0.005,
-          decoration: BoxDecoration(
-            color: lineColor ?? Theme.of(context).colorScheme.primary,
-          ),
-        ),
-        Center(
-          child: Container(
-            width: (lineWidth ?? MediaQuery.of(context).size.width * 0.35) * 0.04,
-            height: (lineWidth ?? MediaQuery.of(context).size.width * 0.35) * 0.04,
+    return Padding(
+      padding: dividerPadding ?? const EdgeInsets.symmetric(
+        vertical: 12
+      ),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            width: lineWidth ?? MediaQuery.of(context).size.width * 0.35,
+            height: (lineWidth ?? MediaQuery.of(context).size.width * 0.35) * 0.005,
             decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.background,
-              shape: BoxShape.circle,
-              border: Border.all(
-                color: circleBorderColor ?? Theme.of(context).colorScheme.primary
-              )
+              color: lineColor ?? Theme.of(context).colorScheme.primary,
             ),
           ),
-        )
-      ],
+          Center(
+            child: Container(
+              width: (lineWidth ?? MediaQuery.of(context).size.width * 0.35) * 0.04,
+              height: (lineWidth ?? MediaQuery.of(context).size.width * 0.35) * 0.04,
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.background,
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: circleBorderColor ?? Theme.of(context).colorScheme.primary
+                )
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
